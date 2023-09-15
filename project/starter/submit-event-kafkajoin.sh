713c7aa8c755b969738060d68e6a7f1fc23bfd42
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-09-15 13:48:18 +0200

    Update pathes

--- project/starter/submit-event-kafkajoin.sh
+++ project/starter/submit-event-kafkajoin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it spark-nanodegree-human-balance_spark_1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/thomas/IdeaProjects/Spark-Nanodegree-human-balance/project/starter/submit-event-kafkajoin.sh | tee ../../spark/logs/kafkajoin.log
+docker exec -it spark-nanodegree-human-balance-project_spark_1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/thomas/IdeaProjects/Spark-Nanodegree-human-balance-project/project/starter/sparkpykafkajoin.py | tee ../../spark/logs/kafkajoin.log
--- project/starter/submit-event-kafkastreaming.sh
+++ project/starter/submit-event-kafkastreaming.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it nd029-c2-apache-spark-and-spark-streaming-starter_spark_1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/thomas/IdeaProjects/Spark-Nanodegree-human-balance/project/starter/sparkpyeventskafkastreamtoconsole.py | tee ../../spark/logs/eventstream.log
+docker exec -it spark-nanodegree-human-balance-project_spark_1 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/thomas/IdeaProjects/Spark-Nanodegree-human-balance-project/project/starter/sparkpyeventskafkastreamtoconsole.py | tee ../../spark/logs/eventstream.log
